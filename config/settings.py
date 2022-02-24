3288c73d484852bf3339e8facd08a313e1265734
José Arias <ariasc.j95@gmail.com>
2022-02-24 00:25:30 -0600

    New updates for Heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k_ap=xzp&b0a4x@2db484qvy%__uib1w4anyh_*lxdkm(p_-io'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
